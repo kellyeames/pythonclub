1c475d552944dfb9aee8b8aab6a5d30168e4bf0e
kellyeames <46878406+kellyeames@users.noreply.github.com>
2019-05-23 11:09:41 -0700

    Add files via upload

---
+++ clubapp/tests.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from django.urls import reverse
+from .models import Meeting, Resource, MeetingMinutes, Event
+from django.contrib.auth.models import User
+
+# Create your tests here.
+class MeetingTest(TestCase):
+    def test_string(self):
+        type=Meeting(meetingtitle="meetingtitle")
+        self.assertEqual(str(type), type.meetingtitle)
+    
+    def test_table(self):
+        self.assertEqual(str(Meeting._meta.db_table), 'meeting')
+
+class Resource(TestCase):
+    def test_string(self):
+        type=Resource(resourcename="resourcename")
+        self.assertEqual(str(type), type.resourcename)
+    
+    def test_table(self):
+        self.assertEqual(str(Resource._meta.db_table), 'resource')
+
+
+
